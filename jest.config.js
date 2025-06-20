9f6cdca376511316a806669b5af0b1a34a6fe426
car06 <221220@ids.upchiapas.edu.mx>
2025-06-19 22:38:06 -0600

    Jest was installed for testing - fixes #7

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+export default {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  moduleNameMapping: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  testMatch: [
+    '<rootDir>/src/**/__tests__/**/*.{js,jsx}',
+    '<rootDir>/src/**/*.{test,spec}.{js,jsx}'
+  ]
+};
